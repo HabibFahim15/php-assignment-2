1d010cc618ecfa70974baa89ca3e35764073a08e
Habib Hossain <habibfahim71@gmail.com>
2025-07-12 22:09:29 +0600

    first commit

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+
+$contacts = [];
+
+function addContact(array &$contacts  , string $name, string $email, $phone):void 
+{
+    $contacts[] = ['name' => $name, 'email' => $email, 'phone' => $phone];
+    // array_push($contacts, ['name' => $name, 'email' => $email, 'phone' => $phone]);
+}
+
+function displayContacts(array $contacts):void 
+{
+    foreach ($contacts as $contact) {
+        echo 'Name: ' . $contact['name'] . '<br>';
+        echo 'Email: ' . $contact['email'] . '<br>';
+        echo 'Phone: ' . $contact['phone'] . '<br>';
+    }
+}
